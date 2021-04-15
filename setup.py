3e6d7b9b8b5c0c136468ac3edde4d457d542ddaf
jeovane16 <jeovane.araujo@opolen.com.br>
2021-04-15 17:59:07 -0300

    configuração inicial do package

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="polen_charity_donation_api",
+    version="1.0.0",
+    author="polen",
+    author_email="eupolinizo@opolen.com.br",
+    description="A sua API de doações",
+    long_description=open('README.md').read(),
+    url="https://polen-donation.github.io/polen-docs/",
+    project_urls={
+        "Bug Tracker": "https://github.com/Polen-Donation/polen-charity-donation-api/issues",
+        "Documentation": "https://polen-donation.github.io/polen-docs/",
+        "Source": "https://github.com/Polen-Donation/polen-charity-donation-api",
+    },
+    keywords=['charity', 'OSC', 'ODS', 'Ongs', 'Ngos', 'Corporate', 'Donation', 'Doação', 'crowdfunding', 'Funding', 'Impact', 'Social', 'Giving'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=2.7",
+    install_requires=['requests']
+)
