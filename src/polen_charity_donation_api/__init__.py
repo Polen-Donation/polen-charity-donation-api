33e976d852f5c20ec12333104c4e6e554ebe181b
jeovane16 <jeovane.araujo@opolen.com.br>
2021-04-16 10:04:21 -0300

    adicionado o modulo de cause

--- src/polen_charity_donation_api/__init__.py
+++ src/polen_charity_donation_api/__init__.py
@@ -1,9 +1,43 @@
 import requests
 import json
 
+
 class PolenCharityDonationAPI:
     base_url = 'https://api.polen.com.br/api/v2'
 
     def __init__(self, api_token: str):
         self.api_token = api_token
 
+        # Cause
+
+    def get_all_cause(self, params={}):
+        """Método que retorna todas as causas cadastradas
+            Parameter
+            ----------
+              params: dict
+                Dicionário com as configurações adicionais
+        """
+
+        params['api_token'] = self.api_token
+        return requests.get(f'{self.base_url}/cause/all', params=params)
+
+    def get_all_categories(self, params={}):
+        """Método que retorna todas as categorias cadastradas
+              Parameter
+              ----------
+                params: dict
+                  Dicionário com as configurações adicionais
+        """
+
+        params['api_token'] = self.api_token
+        return requests.get(f'{self.base_url}/cause/categories', params=params)
+
+    def get_own_cause(self, params: dict):
+        """Método que retorna todas as causas de uma loja
+            Parameter
+            ----------
+              params: dict
+                Dicionário com as configurações adicionais
+        """
+        params['api_token'] = self.api_token
+        return requests.get(f'{self.base_url}/cause', params=params)
