dd41eb2c2c320129f6b337e263a30413727f34d4
jeovane16 <jeovane.araujo@opolen.com.br>
2021-04-16 10:08:38 -0300

    adicionado o modulo de company

--- src/polen_charity_donation_api/__init__.py
+++ src/polen_charity_donation_api/__init__.py
@@ -8,7 +8,7 @@ class PolenCharityDonationAPI:
     def __init__(self, api_token: str):
         self.api_token = api_token
 
-        # Cause
+    # Cause
 
     def get_all_cause(self, params={}):
         """Método que retorna todas as causas cadastradas
@@ -41,3 +41,60 @@ class PolenCharityDonationAPI:
         """
         params['api_token'] = self.api_token
         return requests.get(f'{self.base_url}/cause', params=params)
+
+    # Company
+
+    def get_company_detail(self, params: dict):
+        """Método que retorna os detalhes de uma empresa
+            Parameter
+            ---------
+              params: dict
+                Dicionário com as configurações adicionais
+        """
+        params['api_token'] = self.api_token
+        return requests.get(f'{self.base_url}/company/detail', params=params)
+
+    def get_company_list(self, params: dict):
+        """Método que retorna uma lista com as empresas de uma conta
+            Parameter
+            ---------
+              params: dict
+                Dicionário com as configurações adicionais
+        """
+        params['api_token'] = self.api_token
+        return requests.get(f'{self.base_url}/company/list', params=params)
+
+    def get_company_stores(self, params: dict):
+        """Método que retorna uma lista de lojas de uma empresa
+            Parameter
+            ---------
+              params: dict
+                Dicionário com as configurações adicionais
+        """
+        params['api_token'] = self.api_token
+        return requests.get(f'{self.base_url}/company/stores', params=params)
+
+    def update_company(self, body: dict, params: dict):
+        """Método que atualiza uma empresa
+            Parameter
+            ---------
+              params: dict
+                Dicionário com as configurações adicionais
+              body: dict
+                Dicionário com o corpo da requisição
+        """
+        params['api_token'] = self.api_token
+        return requests.put(f'{self.base_url}/company/update', params=params, data=json.dump(body))
+
+    def create_company(self, body: dict, params={}):
+        """Método que cria uma nova empresa
+
+            Parameter
+            ---------
+              params: dict
+                Dicionário com as configurações adicionais
+              body: dict
+                Dicionário com o corpo da requisição
+        """
+        params['api_token'] = self.api_token
+        return requests.post(f'{self.base_url}/company/update', params=params, json=body)
